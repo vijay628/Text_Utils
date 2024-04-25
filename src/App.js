95e2f2b17a79067e9f67033ea4386930de116252
vijay628 <durgvijaymaurya21@gmail.com>
2024-04-25 15:51:06 +0530

    dark mode updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import './App.css';
 import TextForm from './components/TextForm';
 import Navbar from './components/Navbar';
+import React, { useState } from 'react'
 
 function App() {
+const [clicked, setClicked] = useState(true)
+const [about, setabout] = useState(false);
+const [mode, setMode] = useState("dark")
+const showAbout = () =>{
+setabout(true);
+setClicked(false);
+};
+const toggelMode = () =>{
+if(mode === "dark"){
+setMode("light");
+document.body.style.backgroundColor = "white"
+}
+else{
+  setMode("dark");
+  document.body.style.backgroundColor = "gray"
+}
+};
   return (
     <div >
-      <Navbar title="TextUtils"/>
-      <div className='container my-3'>
-      <TextForm heading="Enter your text to analyze" />
-      </div>
+      <Navbar title="TextUtils" showAbout={()=>showAbout} about={about} mode={mode} toggelMode={toggelMode}/>
+      {clicked && (
+           <div className='container my-3'>
+           <TextForm heading="Enter your text to analyze" />
+           </div>
+        )};
+       
     </div>
   );
 }
